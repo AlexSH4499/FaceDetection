10f1b0db2abb59b0d044a387b887d19536073e3e
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2018-09-23 14:37:01 -0400

    My own branch for stuff to mess with.

---
+++ openCVSamplefacedetection/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,134 @@
+C:/Users/Familia/Documents/GitHub/UAV/face-detection/openCVSamplefacedetection/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/detection_based_tracker/DetectionBasedTracker_jni.o: \
+  C:/OpenCV-android-sdk/samples/face-detection/jni/DetectionBasedTracker_jni.cpp \
+  C:/OpenCV-android-sdk/samples/face-detection/jni\DetectionBasedTracker_jni.h \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvdef.h \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/hal/interface.h \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cv_cpu_dispatch.h \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/version.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/base.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/opencv_modules.hpp \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\climits \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvstd.hpp \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstring \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cctype \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdlib \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/ptr.inl.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/neon_utils.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/traits.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/matx.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/saturate.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/fast_math.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/types.hpp \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cfloat \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\limits \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/mat.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/bufferpool.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/mat.inl.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/persistence.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/operations.hpp \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdio \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvstd.inl.hpp \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/utility.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/core_c.h \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/types_c.h \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/optim.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/ovx.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect/detection_based_tracker.hpp \
+  /OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect/objdetect_c.h \
+  C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque
+
+C:/OpenCV-android-sdk/samples/face-detection/jni\DetectionBasedTracker_jni.h:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvdef.h:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/hal/interface.h:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cv_cpu_dispatch.h:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/version.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/base.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/opencv_modules.hpp:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\climits:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvstd.hpp:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstring:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cctype:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdlib:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/ptr.inl.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/neon_utils.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/traits.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/matx.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/saturate.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/fast_math.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/types.hpp:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cfloat:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\limits:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/mat.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/bufferpool.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/mat.inl.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/persistence.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/operations.hpp:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstdio:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/cvstd.inl.hpp:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/utility.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/core_c.h:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/types_c.h:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/optim.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/core/ovx.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect/detection_based_tracker.hpp:
+
+/OpenCV-android-sdk/sdk/native/jni/include\opencv2/objdetect/objdetect_c.h:
+
+C:/Users/Familia/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque:
